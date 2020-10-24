971d5d43ac9fd87cd51ad5d4c6c476d5053308a7
aa <ttt@ttt.ttt>
2020-10-24 21:52:33 +0300

    Add state LEDs and refactor

--- Blink.py
+++ Blink.py
@@ -5,70 +5,37 @@ import logging
 import datetime
 import time
 import configparser
+import SpiConnector
 
 
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
-def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-    if (adcnum > 7) or (adcnum < 0):
-        return -1
-    GPIO.output(cspin, True)
-
-    GPIO.output(clockpin, False)  # start clock low
-    GPIO.output(cspin, False)  # bring CS low
-
-    commandout = adcnum
-    commandout |= 0x18  # start bit + single-ended bit
-    commandout <<= 3  # we only need to send 5 bits here
-    for i in range(5):
-        if commandout & 0x80:
-            GPIO.output(mosipin, True)
-        else:
-            GPIO.output(mosipin, False)
-        commandout <<= 1
-        GPIO.output(clockpin, True)
-        GPIO.output(clockpin, False)
-
-    adcout = 0
-    # read in one empty bit, one null bit and 10 ADC bits
-    for i in range(12):
-        GPIO.output(clockpin, True)
-        GPIO.output(clockpin, False)
-        adcout <<= 1
-        if GPIO.input(misopin):
-            adcout |= 0x1
-
-    GPIO.output(cspin, True)
-
-    adcout >>= 1  # first bit is 'null' so drop it
-    return adcout
-
-
-HUMIDITY_LEVEL = 685
-PUMP_DURATION = 3  # 3sec == 100ml
-
-SPICLK = 23
-SPIMISO = 21
-SPIMOSI = 19
-SPICS = 24
+SPI_CLK = 23
+SPI_MISO = 21
+SPI_MOSI = 19
+SPI_CS = 24
 RELAY = 11
+ALARM_LED = 32
+ON_LED = 22
 
 # set up the SPI interface pins
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(SPIMOSI, GPIO.OUT)
-GPIO.setup(SPIMISO, GPIO.IN)
-GPIO.setup(SPICLK, GPIO.OUT)
-GPIO.setup(SPICS, GPIO.OUT)
+GPIO.setup(SPI_MOSI, GPIO.OUT)
+GPIO.setup(SPI_MISO, GPIO.IN)
+GPIO.setup(SPI_CLK, GPIO.OUT)
+GPIO.setup(SPI_CS, GPIO.OUT)
 GPIO.setup(RELAY, GPIO.OUT)
+GPIO.setup(ALARM_LED, GPIO.OUT, GPIO.PUD_OFF, GPIO.LOW)
+GPIO.setup(ON_LED, GPIO.OUT, GPIO.PUD_OFF, GPIO.HIGH)
 
 # 10k trim pot connected to adc #0
 potentiometer_adc = 0
 logging.basicConfig(filename="test.log", level=logging.DEBUG)
 config = configparser.ConfigParser()
+previous_humidity = 0
 
 while True:
     try:
         config.read("config.properties")
-        current_humidity = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
+        current_humidity = SpiConnector.readadc(potentiometer_adc, SPI_CLK, SPI_MOSI, SPI_MISO, SPI_CS)
 
         now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
         logging.debug("%s, value = %s. dry_level: %s", now, str(current_humidity), config["DEFAULT"]["DRY_LEVEL"])
@@ -81,8 +48,14 @@ while True:
             GPIO.output(RELAY, GPIO.HIGH)
             time.sleep(int(config["DEFAULT"]["PUMP_DURATION"]))
             GPIO.output(RELAY, GPIO.LOW)
+            if previous_humidity > int(config["DEFAULT"]["DRY_LEVEL"]):
+                print(">>>>> WANRNIG: check system work \n")
+                GPIO.output(ALARM_LED, GPIO.HIGH)
+        else:
+            GPIO.output(ALARM_LED, GPIO.LOW)
 
-        # wait 60 min
+        previous_humidity = current_humidity
+        # wait 60 min (3600)
         time.sleep(3600)
     except KeyboardInterrupt:
         print("Program close...")
